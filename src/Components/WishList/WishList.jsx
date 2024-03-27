f5de9ce4b7a6298151d2406fd271f1a667bba265
unknown <sanjidaema81@gmail.com>
2024-03-28 00:53:19 +0600

    created wishlist

--- src/Components/WishList/WishList.jsx
+++ src/Components/WishList/WishList.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import ShowWishList from "../ShowWishList/ShowWishList";
 
 
@@ -7,8 +6,6 @@ import ShowWishList from "../ShowWishList/ShowWishList";
 const WishList = () => {
 
     const [bookList, setBookList] = useState([]);
-
-    // console.log(bookList);
 
     useEffect(() => {
 
@@ -22,16 +19,8 @@ const WishList = () => {
         <div>
             <h2>This is WishList</h2>
             {
-                bookList.map(book => <ShowWishList key={book.bookId}></ShowWishList>)
+                bookList.map(book => <ShowWishList key={book.bookId} book={book}></ShowWishList>)
             }
-
-{/* {
-    bookList.map(book => <li key={book.bookId}>{book.bookName}</li>)
-} */}
-
-            <div className="mt-16">
-                <Link  className="bg-green-600 p-3 rounded-xl text-white font-bold" to='/home'>Go back to Home</Link>
-            </div>
         </div>
     );
 };
